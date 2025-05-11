5e810761cd2f8c70be275e856d590e877ba35186
Sam-Cheong <111277315+Sam-Cheong@users.noreply.github.com>
2025-05-11 20:03:34 +0800

    Update footer.php

--- food-order-project/food-order/admin/partials/footer.php
+++ food-order-project/food-order/admin/partials/footer.php
@@ -1,7 +1,7 @@
 <!-- Footer Section Starts -->
 <div class="footer">
             <div class="wrapper">
-                <p class="text-center">CISC3003 Team Project Individual Part AgustinYan DC125714</a></p>
+                <p class="text-center">CISC3003 Individual Part | Cheong Iok Cheong DC226373</a></p>
             </div>
         </div>
         <!-- Footer Section Ends -->
