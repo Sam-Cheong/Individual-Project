bb8f3aa5476aec2951e2e4b6105bc3dd56392e19
Sam-Cheong <111277315+Sam-Cheong@users.noreply.github.com>
2025-05-11 18:16:10 +0800

    Add files via upload

---
+++ food-order-project/food-order/admin/partials/footer.php
@@ -0,0 +1,10 @@
+<!-- Footer Section Starts -->
+<div class="footer">
+            <div class="wrapper">
+                <p class="text-center">CISC3003 Team Project Individual Part AgustinYan DC125714</a></p>
+            </div>
+        </div>
+        <!-- Footer Section Ends -->
+
+    </body>
+</html>
